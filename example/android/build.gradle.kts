e2117e8c800e11b741a386903fa84d28c3fb4ff6
Jules Tréhorel <jules.trehorel@iadvize.com>
2025-09-22 17:19:03 +0200

    (build) fix Android SDK repo URL

--- example/android/build.gradle.kts
+++ example/android/build.gradle.kts
@@ -2,7 +2,7 @@ allprojects {
     repositories {
         google()
         gradlePluginPortal()
-        maven(url = uri("https://raw.github.com/iadvize/iadvize-android-sdk/master"))
+        maven(url = uri("https://raw.githubusercontent.com/iadvize/iadvize-android-sdk/master"))
         maven(url = uri("https://plugins.gradle.org/m2/"))
         maven(url = uri("https://jitpack.io"))
         mavenCentral()
